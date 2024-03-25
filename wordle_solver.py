c9fa095bdf7c338317944210fd7f8a587830fe9d
05JDR <jqselmaq@gmail.com>
2024-03-25 19:26:07 -0400

    Current ver requires user input for 1st word (ver 0.8) it does do a semi okay job of complete solutions though.

--- wordle_solver.py
+++ wordle_solver.py
@@ -14,7 +14,7 @@ class Algorithm(object):
     def __init__(self):
         self.x = WordFilter("dictionary.txt").get_valid_words()
 
-    def find_least_worst_case_options(self, full_list):
+    def find_least_worst_case_options(self, full_list, past_list=None):
         self.base_value = len(full_list)
         for i in range(len(full_list)):
             self.guess_options = []
@@ -30,19 +30,21 @@ class Algorithm(object):
                 self.smallest_list_of_options = self.guess_options.copy()
                 self.base_value = len(self.guess_options)
                 self.store_word = full_list[i]
-        return self.store_word    
+        return self.store_word
+        
     
-    def manage_info(self, prev_word):
+    def manage_info(self, prev_word, list_letters_thrown_away=[]):
          dict_confirmed_letters, dict_immovable_letters = {}, {}
-         list_letters_thrown_away, pass_1, pass_2, pass_3 = [], [], [], []
-         x = input(f"Enter your results from {prev_word} Key: enter 0 for gray, 1 for yellow, and 2 for green.\n")
+         pass_1, pass_2, pass_3 = [], [], []
+         x = input(f"Enter your results from '{prev_word}' Key: enter 0 for gray, 1 for yellow, and 2 for green.\n")
          for c in range(len(x)):
+             if x[c] == "2":
+                 dict_immovable_letters[c] = prev_word[c]
              if x[c] == "1":
                 dict_confirmed_letters[c] = prev_word[c]
              if x[c] == "0":
-                list_letters_thrown_away.append(prev_word[c])
-             if x[c] == "2":
-                 dict_immovable_letters[c] = prev_word[c]
+                if prev_word[c] not in dict_immovable_letters.values() and prev_word[c] not in list_letters_thrown_away:
+                    list_letters_thrown_away.append(prev_word[c])
          if x == "00000":
             for word in self.smallest_list_of_options:
                 check_all_letters = True
@@ -68,6 +70,9 @@ class Algorithm(object):
                     if char not in word:
                         check_all_letters = False
                         break
+                 for idx in dict_confirmed_letters.keys():
+                     if word[idx] == dict_confirmed_letters[idx]:
+                         check_all_letters = False
                  if check_all_letters:
                      pass_2.append(word)
 
@@ -78,8 +83,12 @@ class Algorithm(object):
                          check_all_letters = False
                  if check_all_letters:
                      pass_3.append(word)
-             print(self.find_least_worst_case_options(pass_3))
-                
+             if x != "22222":
+                print(f"Next suggestion: {self.find_least_worst_case_options(pass_3)}")
+                self.manage_info(self.find_least_worst_case_options(pass_3), list_letters_thrown_away)
+             else:
+                print(f"{self.find_least_worst_case_options(pass_3)} is the correct answer yay!")
+    
                    
 
                  
@@ -94,6 +103,6 @@ class Algorithm(object):
 
 
 start = time.time()            
-print(Algorithm().manage_info("naieo"))
+Algorithm().manage_info("arose")
 end = time.time()
 print(f"total time elapsed: {end-start}")
